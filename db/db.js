4ba92394410d0a855bf279c92fdcf46cdff80b56
Jericho Sharman <jsharman@hotmail.com.au>
2024-02-13 16:06:23 +1100

    mysql connection working now for this new user

--- db/db.js
+++ db/db.js
@@ -8,7 +8,7 @@ require('dotenv').config();
 
 // Create the connection 
 const db = mysql.createConnection({
-    connectionLimit: 5,             // Adjust this value as needed 2 connections 1 for the app and 1 for express.sessions
+    connectionLimit: 15,             // Adjust this value as needed 2 connections 1 for the app and 1 for express.sessions
     host: process.env.DB_HOST,      // Replace with correct host ip
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
